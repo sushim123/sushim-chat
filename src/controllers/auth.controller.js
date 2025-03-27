ea7a5c1c98c3716e06b7a82ac12aa7607586c887
sushim123 <sushimpaddd@gmail.com>
2025-03-27 13:44:15 +0530

    logOut route added with remmoving cookie and tested succesfully in postman

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -18,7 +18,7 @@ export const signup = async (req, res) => {
       password: hashedPassword,
     });
     if (newUser) {
-      generateToken(newUser._id,res)
+      generateToken(newUser._id, res);
       await newUser.save();
       console.log(newUser);
       res.status(201).json({
@@ -36,7 +36,6 @@ export const signup = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  
   const { email, password } = req.body;
   try {
     const user = await User.findOne({ email });
@@ -47,20 +46,29 @@ export const login = async (req, res) => {
     if (!isPasswordCorrect) {
       return res.status(400).json({ message: "Invalid Credentials" });
     }
-    generateToken(user._id,res);
+    generateToken(user._id, res);
     res.status(200).json({
       _id: user._id,
       fullName: user.fullName,
       email: user.email,
       password: user.password,
     });
-    console.log("login successful")
+    console.log("login successful");
   } catch (error) {
     console.error("Error in login", error.message);
     res.status(500).json({ message: "internal server error" });
   }
 };
-
+export const logout = async (req, res) => {
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+    console.log("Logged out successfully");
+  } catch (error) {
+    console.log("Error in Logging Out controller", error.message);
+    res.status(500).json({ message: "internal server error" });
+  }
+};
 export const updateProfile = async (req, res) => {
   try {
     const { profilePic } = req.body;
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { signup, login ,updateProfile, checkAuth} from "../controllers/auth.controller.js";
+import { signup, login ,updateProfile, checkAuth, logout} from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.post("/logout", logout);
 router.post("/update-profile",protectRoute,updateProfile);
 router.get("/check",protectRoute,checkAuth);
 
