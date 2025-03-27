7e3fbd894125225e6e9e8c24a0ecb2597677eaba
sushim123 <sushimpaddd@gmail.com>
2025-03-27 18:03:25 +0530

    Add sendMessage controller and route for sending messages with optional image upload

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import User from "../modules/user.model.js";
 import Message from "../modules/message.model.js";
+import cloudinary from "cloudinary";
 
 export const getUsersForSidebar = async (req, res) => {
   try {
@@ -29,6 +30,33 @@ export const getMessages = async (req, res) => {
     res.status(200).json(message);
   } catch (error) {
     console.log("Error in getMessages function", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+    
+    await newMessage.save();
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("error in sendMessage controller", error.message);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
--- src/routes/message.routes.js
+++ src/routes/message.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getMessages, getUsersForSidebar } from "../controllers/message.controller.js";
+import { getMessages, getUsersForSidebar ,sendMessage } from "../controllers/message.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const messageRoutes = express.Router();
 
 messageRoutes.get("/users",protectRoute, getUsersForSidebar);
 messageRoutes.get("/:id",protectRoute, getMessages);
+messageRoutes.post("/send/:id",protectRoute, sendMessage);
 export default messageRoutes;
