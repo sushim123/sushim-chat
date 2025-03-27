97f910bec162247eaeb252b5f03d93d90424bfad
sushim123 <sushimpaddd@gmail.com>
2025-03-27 16:55:14 +0530

    Add CORS middleware support to the application

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import express from "express";
 import router from "./routes/auth.routes.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
+
 import { mongoDB } from "./lib/db.js";
 import messageRoutes from "./routes/message.routes.js";
 dotenv.config();
@@ -9,6 +12,8 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
+
 app.use("/api/auth", router);
 app.use("/api/message", messageRoutes);
 
