2efd991e3b339dab2c717c10059ff6f2bf0fb968
sushim123 <sushimpaddd@gmail.com>
2025-03-27 16:14:44 +0530

    Change update-profile route method from POST to PUT

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
-router.post("/update-profile",protectRoute,updateProfile);
+router.put("/update-profile",protectRoute,updateProfile);
 router.get("/check",protectRoute,checkAuth);
 
 export default router;
