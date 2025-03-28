44bbe86438177ccbe66f0724b60bdb95e27202e9
sushim123 <sushimpaddd@gmail.com>
2025-03-28 17:53:32 +0530

    add axios instance and authentication store; implement auth checks in routing

--- sushimChat/src/App.jsx
+++ sushimChat/src/App.jsx
@@ -1,20 +1,48 @@
 import Navbar from "./components/Navbar";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import SignUpPage from "./pages/SignUpPage";
 import LogInPage from "./pages/LogInPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
+import { Loader } from "lucide-react";
+import { useAuthStore } from "./store/useAuthStore.js";
+import { useEffect } from "react";
 const App = () => {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+  console.log({ authUser });
+  if (isCheckingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen ">
+        <Loader className="size-20 animate-spin" />
+      </div>
+    );
+
   return (
     <div>
       <Navbar />
+
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LogInPage />} />
+        <Route
+          path="/"
+          element={authUser ? <Homepage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LogInPage /> : <Navigate to="/" />}
+        />
         <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
       </Routes>
     </div>
   );
---
+++ sushimChat/src/store/useAuthStore.js
@@ -0,0 +1,22 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios.js";
+
+export const useAuthStore = create((set) => ({
+  authUser: null,
+  isSigningUp: false,
+  isloggingUp: false,
+  isUpdatingProfile: false,
+
+  isCheckingAuth: true,
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
+      set({ authUser: res.data });
+    } catch (error) {
+      console.log("error in checkAuth function", {message:error.message});
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+}));
