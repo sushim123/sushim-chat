db872e20c6c547746377741762cd2c4165cd6a2f
sushim123 <sushimpaddd@gmail.com>
2025-04-01 00:46:52 +0530

    signup form is completed with all validation with backend post method and designed signup with toast success and error message

--- sushimChat/src/App.jsx
+++ sushimChat/src/App.jsx
@@ -5,9 +5,11 @@ import SignUpPage from "./pages/SignUpPage";
 import LogInPage from "./pages/LogInPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
-import { Loader } from "lucide-react";
 import { useAuthStore } from "./store/useAuthStore.js";
 import { useEffect } from "react";
+import { motion } from "framer-motion";
+import {Toaster} from"react-hot-toast"
+
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
   useEffect(() => {
@@ -16,10 +18,26 @@ const App = () => {
   console.log({ authUser });
   if (isCheckingAuth && !authUser)
     return (
-      <div className="flex items-center justify-center h-screen ">
-        <Loader className="size-20 animate-spin" />
+      <div className="flex justify-center items-center h-screen bg-gray-900">
+        <div className="flex space-x-2">
+          {[0, 1, 2].map((i) => (
+            <motion.span
+              key={i}
+              className="w-4 h-4 bg-blue-500 rounded-full"
+              animate={{
+                y: [0, -10, 0],
+              }}
+              transition={{
+                duration: 0.6,
+                repeat: Infinity,
+                delay: i * 0.2,
+              }}
+            />
+          ))}
+        </div>
       </div>
     );
+  
 
   return (
     <div>
@@ -44,6 +62,7 @@ const App = () => {
           element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
         />
       </Routes>
+      <Toaster/>
     </div>
   );
 };
--- sushimChat/src/store/useAuthStore.js
+++ sushimChat/src/store/useAuthStore.js
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
+import toast from "react-hot-toast";
 
 export const useAuthStore = create((set) => ({
   authUser: null,
@@ -13,10 +14,31 @@ export const useAuthStore = create((set) => ({
       const res = await axiosInstance.get("/auth/check");
       set({ authUser: res.data });
     } catch (error) {
-      console.log("error in checkAuth function", {message:error.message});
+      console.log("error in checkAuth function", { message: error.message });
       set({ authUser: null });
     } finally {
       set({ isCheckingAuth: false });
+    }
+  },
+  signup: async (data) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await axiosInstance.post("/auth/signup", data);
+      set({ authUser: res.data });
+      toast.success("Account Created Successfully");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isSigningUp: false });
+    }
+  },
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("LOgout successfully");
+    } catch (error) {
+      toast.error(error.response.data.message);
     }
   },
 }));
