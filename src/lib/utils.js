d2ee2ee441b0f6c214fe4fd033c51f24029243a2
sushim123 <sushimpaddd@gmail.com>
2025-04-08 14:46:53 +0530

    rename sameSite to None

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,13 +4,15 @@ const generateToken = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
+
   res.cookie("jwt", token, {
-    secure: process.env.NODE_ENV === "production",
-    maxAge: 7 * 24 * 60 * 60 * 1000,
+    secure: process.env.NODE_ENV === "production", 
+    maxAge: 7 * 24 * 60 * 60 * 1000, 
     httpOnly: true,
-    samesite: "none",
-    
+    sameSite: "None", 
   });
+
   return token;
 };
+
 export default generateToken;
