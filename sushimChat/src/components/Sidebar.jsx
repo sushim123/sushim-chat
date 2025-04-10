f695b1d893203d18c6d5476c82eef05a7914a11c
sushim123 <sushimpaddd@gmail.com>
2025-04-10 23:46:43 +0530

    sidebar profileImage default added

--- sushimChat/src/components/Sidebar.jsx
+++ sushimChat/src/components/Sidebar.jsx
@@ -3,6 +3,7 @@ import { useChatStore } from "../store/useChatStore";
 import SidebarSkeleton from "../components/skeleton/SidebarSkeleton";
 import { useAuthStore } from "../store/useAuthStore";
 import { Users } from "lucide-react";
+import profileImage from "../assets/profile.png";
 
 const Sidebar = () => {
   const { getUsers, users, selectedUser, setSelected, isUsersLoading } =
@@ -39,7 +40,7 @@ const Sidebar = () => {
           >
             <div className="relative mx-auto lg:mx-0">
               <img
-                src={user.profilePic || "src/assets/profile.png"}
+                src={user.profilePic || profileImage}
                 alt={user.name}
                 className="size-12 object-cover rounded-full"
               />
