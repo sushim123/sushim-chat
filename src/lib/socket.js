7a54277c5adff037bfac742bd9cafbcada811987
sushim123 <sushimpaddd@gmail.com>
2025-04-10 23:37:54 +0530

    Update CORS configuration to allow requests from the production URL

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://sushim-chat.vercel.app"],
     methods: ["GET", "POST"],
     credentials:true
   },
