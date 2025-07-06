4790cbfff0dca5cb3bf701a2e7dd652410e2ce68
sushim123 <sushimpaddd@gmail.com>
2025-07-06 12:47:55 +0530

    refactor: remove unused components and files
    - Deleted ChatHeader, MessageInput, Navbar, NoChatSelected, Sidebar, and their respective skeleton components.
    - Removed unused utility functions and axios configuration.
    - Cleaned up authentication and chat store logic by removing unnecessary code.
    - Deleted all pages related to login, signup, profile, and settings.
    - Removed global CSS imports and configurations.
    - Cleaned up Vite configuration file.

--- sushimChat/src/lib/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const baseURL ="https://sushim-chat.onrender.com/api";
-export const socketBaseURL = "https://sushim-chat.onrender.com"
-export const axiosInstance = axios.create({
-  baseURL,
-  withCredentials: true,
-});
