b611e9737e839678d22cfea45d6ae9535e5978ee
sushim123 <sushimpaddd@gmail.com>
2025-07-27 20:13:25 +0530

    chat Messages on rightClick options come to select

--- sushimchatapp/src/components/ChatMessages.tsx
+++ sushimchatapp/src/components/ChatMessages.tsx
@@ -1,11 +1,52 @@
 import { useAuthStore } from "@/store/useAuthStore";
 import { useChatStore } from "@/store/useChatStore";
-import { useEffect, useRef } from "react";
-
+import { useEffect, useRef, useState } from "react";
+import deleteIcon from "../../public/delete.svg";
+import copyIcon from "../../public/copy.svg";
+import forwardIcon from "../../public/forward.svg";
+import pinIcon from "../../public/pin.svg";
+import selectIcon from "../../public/select.svg";
 const ChatMessages = () => {
   const { selectedUser, messages, getMessages } = useChatStore();
+  const [clickPosition, setClickPosition] = useState<{
+    x: number;
+    y: number;
+  } | null>(null);
+
+  const menuItems = [
+    {
+      label: "Delete",
+      icon: deleteIcon,
+    },
+    {
+      label: "Copy",
+      icon: copyIcon,
+    },
+    {
+      label: "Forward",
+      icon: forwardIcon,
+    },
+    {
+      label: "Pin",
+      icon: pinIcon,
+    },
+    {
+      label: "Select",
+      icon: selectIcon,
+    },
+  ];
+
   const { authUser } = useAuthStore();
   const bottomRef = useRef<HTMLDivElement | null>(null);
+
+  const handleRightClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setClickPosition({ x: e.pageX, y: e.pageY });
+  };
+
+  const handleCLick = () => {
+    setClickPosition(null);
+  };
   useEffect(() => {
     if (selectedUser?._id) {
       getMessages(selectedUser._id);
@@ -16,11 +57,12 @@ const ChatMessages = () => {
   }, [messages]);
 
   return (
-    <div className="flex flex-col gap-[10px]">
+    <div className="flex flex-col gap-[10px]" >
       {messages?.map((message) => {
         const isSendByMe = message.senderId === authUser?._id;
         return (
           <div
+            onContextMenu={handleRightClick}
             key={message._id}
             className={`w-fit max-w-[70%]  text-white text-[18.2px] opacity-100 px-4 py-[13px] shadow-inner-custom rounded-[30px] bg-custom-gradient relative ${
               isSendByMe ? "self-end" : "self-start"
@@ -41,6 +83,28 @@ const ChatMessages = () => {
         );
       })}
       <div ref={bottomRef}></div>
+      {clickPosition && (
+        <ul
+          onClick={handleCLick}
+          className="absolute  bg-[#060606]  shadow-inner-custom rounded-[18px] w-fit h-fit px-[20px] py-[13px] text-black z-50"
+          style={{
+            top: clickPosition.y,
+            left: clickPosition.x,
+          }}
+        >
+          <div className="gap-[21px] flex flex-col">
+            {menuItems.map((item) => (
+              <div
+                key={item.label}
+                className="flex gap-2 text-2xl items-center"
+              >
+                <img src={item.icon.src} />
+                <li className="text-white">{item.label}</li>
+              </div>
+            ))}
+          </div>
+        </ul>
+      )}
     </div>
   );
 };
