17442c30b9bd36aec48abd6fb6eb9236b4a8aca5
sushim123 <sushimpaddd@gmail.com>
2025-07-31 22:33:12 +0530

    socket url changed

--- sushimchatapp/src/lib/axios.ts
+++ sushimchatapp/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const baseURL = "/api";
-export const socketBaseURL = "https://sushim-chat.onrender.com";
+export const socketBaseURL = "https://sushim-chat-kc00.onrender.com";
 export const axiosInstance = axios.create({
   baseURL,
 
